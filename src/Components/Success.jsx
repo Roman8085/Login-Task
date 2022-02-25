44ab0658373a52fcde42726d3bd00c500cddb7c4
Ayush <ayushsinghkhamhariya@gmail.com>
2022-02-25 11:30:41 +0530

    My login task

---
+++ src/Components/Success.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../App.css";
+
+const Success = () => {
+  return (
+    <div className='container'>
+        <div className='app-wrapper'>
+                <h1 className='form-success'>Account is Successfully Created!</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Success
